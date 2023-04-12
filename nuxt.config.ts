101a3859e3527a24f4dfff29bfa42794a779cc42
Nick Taylor <nick@iamdeveloper.com>
2023-04-12 16:27:39 -0400

    chore: cleaned up config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,15 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-const yo = defineNuxtConfig({
+export default defineNuxtConfig({
     nitro: {
     routeRules: {
       '/**': { swr: true },
     },
-    // prerender: {
-    //   routes: ['/**'],
-    // },
   },
 })
-
-console.dir(yo)
-
-export default yo
